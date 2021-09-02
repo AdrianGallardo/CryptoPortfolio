44ce547af3548b258cbea69738a76ad7b3a3d798
Adrian Gallardo <adrian.f.gallardo@gmail.com>
2021-09-02 00:59:43 -0500

    Add SettingsVC Layout

---
+++ CryptoPortfolio/FiatViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FiatViewController.swift
+//  CryptoPortfolio
+//
+//  Created by Adrian Gallardo on 02/09/21.
+//
+
+import Foundation
---
+++ CryptoPortfolio/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  CryptoPortfolio
+//
+//  Created by Adrian Gallardo on 01/09/21.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UITableViewController {
+	
+}
