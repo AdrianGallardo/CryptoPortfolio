e3a74eefbb7a3549b7892dc6e4e561dc2da75784
Adrian Gallardo <adrian.f.gallardo@gmail.com>
2022-01-26 11:53:35 -0600

    Remove extra code comments

--- CryptoPortfolio/ViewControllers/PercentChangeViewcontroller.swift
+++ CryptoPortfolio/ViewControllers/PercentChangeViewcontroller.swift
@@ -11,21 +11,12 @@ import UIKit
 class PercentChangeViewController: UIViewController {
 	@IBOutlet weak var tableView: UITableView!
 
-//	var fiatCurrencies: [FiatData] = []
-//	var lastSelection: IndexPath!
-//	var idFiatCurrencyUserDefault: Int?
-
 	var lastSelection: IndexPath!
 	var timeFrameUserDefault: String?
 	var timeFrames: [String] = ["1h", "24h", "7d", "30d"]
 
 	// MARK: - Lifecycle
 	override func viewDidLoad() {
-//		super.viewDidLoad()
-//		idFiatCurrencyUserDefault = UserDefaults.standard.object(forKey: "idFiatCurrency") as? Int
-//		navigationController?.navigationBar.barTintColor = UIColor(red: 0.14, green: 0.15, blue: 0.15, alpha: 1.00)
-//		navigationController?.navigationBar.tintColor = UIColor.white
-
 		super.viewDidLoad()
 		timeFrameUserDefault = UserDefaults.standard.object(forKey: "timeFrame") as? String
 		navigationController?.navigationBar.barTintColor = UIColor(red: 0.14, green: 0.15, blue: 0.15, alpha: 1.00)
@@ -48,24 +39,10 @@ extension PercentChangeViewController: UITableViewDataSource, UITableViewDelegat
 	}
 
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//		print("fiatCurrencies.count: \(fiatCurrencies.count)")
-//		return fiatCurrencies.count
 		return timeFrames.count
 	}
 
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//		let fiatCurrency = fiatCurrencies[indexPath.row]
-//
-//		// Configure cell
-//		let cell = tableView.dequeueReusableCell(withIdentifier: "timeViewCell") as! TimeViewCell
-//		cell.setFiatCurrency(fiatCurrency: fiatCurrency, accesory: (fiatCurrency.id == idFiatCurrencyUserDefault))
-//
-//		if fiatCurrency.id == idFiatCurrencyUserDefault {
-//			lastSelection = indexPath
-//		}
-//
-//		return cell
-
 		let timeFrame = timeFrames[indexPath.row]
 
 		// Configure cell
@@ -80,17 +57,6 @@ extension PercentChangeViewController: UITableViewDataSource, UITableViewDelegat
 	}
 
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//		if self.lastSelection != nil {
-//			self.tableView.cellForRow(at: self.lastSelection)?.accessoryType = .none
-//		}
-//		self.tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//		self.lastSelection = indexPath
-//		self.tableView.deselectRow(at: indexPath, animated: true)
-//
-//		UserDefaults.standard.set(self.fiatCurrencies[indexPath.row].id, forKey: "idFiatCurrency")
-//		UserDefaults.standard.set(self.fiatCurrencies[indexPath.row].sign, forKey: "signFiatCurrency")
-//		UserDefaults.standard.set(self.fiatCurrencies[indexPath.row].symbol, forKey: "symbolFiatCurrency")
-
 		if self.lastSelection != nil {
 			self.tableView.cellForRow(at: self.lastSelection)?.accessoryType = .none
 		}
